cc1dfd6b950ebc6f64f0115ce7f594a437eb2dff
IcedTeaTech <icedteatech@gmail.com>
2020-11-02 18:14:09 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import "./App.css";
 
 function App() {
+  const api_url = "https://the-one-api.dev/v2";
+
+  const [quotes, setQuotes] = useState([]);
+  const [books, setBooks] = useState([]);
+
+  // useEffect(() => {
+  //   getBooks();
+  // }, []);
+
+  const getBooks = async () => {
+    const response = await fetch(`${api_url}/book`, {
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+    setBooks(data.docs);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container d-flex flex-column align-items-center">
+        {books.map((book) => (
+          <p>{book.name}</p>
+        ))}
+        <div className="blockquote">
+          <Button variant="primary" onClick={getBooks}>
+            Get LOTR books
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
